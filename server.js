76cbca6e2de3e45e32bbae340af02c943676086c
vasergen <vasergen@gmail.com>
2023-02-07 20:04:12 +0100

    create simple expres server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+app.get("/api/test", (req, res) => {
+  return res.send("Hello there!");
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log(`server is listening on port ${PORT}`);
+});
